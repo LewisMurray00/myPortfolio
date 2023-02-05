75c4063174d5726ed170a204ceed97b342f3d335
Lewis Murray <lewisdmurray00@gmail.com>
2023-02-05 23:03:15 +0000

    Added a new motion div

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -29,6 +29,15 @@ const Header = () => {
 
         </div>
       </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className='app__header-img'
+      >
+        <img src={images.profile} alt='profile_bg' />
+      </motion.div>
+
     </div>
   )
 }
