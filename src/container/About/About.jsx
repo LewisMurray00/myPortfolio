ed1b32329d7caa06dd870280c108be125b650ae8
Lewis Murray <lewisdmurray00@gmail.com>
2023-02-12 23:31:15 +0000

    Strted dding some jsx to the about section

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import './About.scss'
+
+import { motion } from 'framer-motion';
+
+const abouts = [
+  {
+    title: 'Web Development',
+    description: 'I am new to web developing but learning new things quickly',
+    imgUrl: 'https://'
+  },
+  {
+    title: 'UI/UX',
+    description: 'I have an understanding of UX/UI',
+    imgUrl: 'https://'
+  }
+];
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <h2 className='head-text'>
+        I Know that 
+        <span>Good design</span>
+        <br />
+        means
+        <span>Good Business</span>
+      </h2>
+
+      <div className='app__profiles'>
+        {abouts.map((about, index)=>{
+          
+        })}
+      </div>
+    </>
   )
 }
 
