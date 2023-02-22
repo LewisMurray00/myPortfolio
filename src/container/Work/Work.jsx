157495457dcd0e025feac1c110d2e3fc62c18595
Lewis Murray <lewisdmurray00@gmail.com>
2023-02-22 17:22:58 +0000

    Created the object array for the first project that will be mapped

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,14 +1,30 @@
 import React, {useState, useEffect} from 'react'
-import { motion } from 'framer-motion';
+import { animate, motion } from 'framer-motion';
 import { AppWrap } from '../../wrapper'
 
 import { AiFillEye, AiFillGithub} from 'react-icons/ai';
+import { images } from '../../constants';
 import './Work.scss'
 
+const work = [
+  {
+    title: 'firstProject',
+    description: 'This is my first project',
+    projectLink: 'www.netlify.com',
+    codeLink: 'www.github.com',
+    imgUrl: images.about01,
+    tags: 'Web App',
+    all: 'all'
+  }
+]
 
 const Work = () => {
 
   const [activeFilter, setactiveFilter] = useState('All')
+  const [animateCard, setanimateCard] = useState({ y: 0, opacity: 1})
+  const [works, setworks] = useState([])
+  const [filterWork, setfilterWork] = useState([])
+
 
   const handleWorkFilter = (item) => {
 
@@ -30,7 +46,13 @@ const Work = () => {
         ))}
       </div>
 
-      
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__work-portfolio"
+      >
+        
+      </motion.div>
     </>
   )
 }
