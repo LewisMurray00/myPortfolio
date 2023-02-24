242ad2d662bf6e962b8eb9cfa1425e65bf93b5e4
Lewis Murray <lewisdmurray00@gmail.com>
2023-02-24 17:14:37 +0000

    Began adding some code to the skills section

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import ReactTooltip from 'react-tooltip';
+
+import { AppWrap } from '../../wrapper';
 import './Skills.scss'
+
+const skillsArray = [
+
+]
+
+const experiencesArray = [
+
+]
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <>
+      <h2 className='head-text'>Skills & Experience</h2>
+
+      <div className='app__skills-container'>
+        <motion.div className='app__skills-list'>
+          {}
+        </motion.div>
+      </div>
+    </>
   )
 }
 
-export default Skills
+export default AppWrap(Skills, 'skills')
