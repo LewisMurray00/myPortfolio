12f7c8b1df7ec338870ba2df5235e41d7fd8772c
Lewis Murray <lewisdmurray00@gmail.com>
2023-03-01 15:24:36 +0000

    Created the testimonials.jsx file adding some html into it, from there i created a handleClick function that cycles through the information that is stored in an array

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -1,10 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi'
+
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { images } from '../../constants';
 import './Testimonial.scss'
 
+const testimonials = [
+  {
+    name: 'Rob',
+    company: 'Spotify',
+    imgUrl: images.spotify,
+    feedback: 'He was good'
+  },
+  {
+    name: 'Bob',
+    company: 'adidas',
+    imgUrl: images.adidas,
+    feedback: 'He was amazing'
+  }
+]
+
 const Testimonial = () => {
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const test = testimonials[currentIndex];
+
+  const handleClick = (index) => {
+    setCurrentIndex(index)
+  }
+
+
   return (
-    <div>Testimonial</div>
+    <>
+      {testimonials.length && (
+        <>
+          <div className='app__testimonial-item app__flex'>
+            <img src={test.imgUrl} alt='testimonials' />
+            <div className='app__testimonial-content'>
+              <p className='p-text'>{test.feedback}</p>
+              <div>
+                <h4 className='bold-text'>{test.name}</h4>
+                <h5 className='p-text'>{test.company}</h5>
+              </div>
+            </div>
+          </div>
+
+          <div className='app__testimonial-btns app__flex'>
+            <div className='app__flex' onClick={(()=> handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1))}>
+              <HiChevronLeft />
+            </div>
+            <div className='app__flex' onClick={(()=> handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1))}>
+              <HiChevronRight />
+            </div>
+          </div>
+        </>
+      )}
+    </>
   )
 }
 
-export default Testimonial
+export default AppWrap(
+  MotionWrap(Testimonial, 'app__testimonial'),
+  'app__primarybg'
+)
