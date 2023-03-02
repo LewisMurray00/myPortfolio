b7c538fa9f8ba82fb2e1769721d1669195f15ea6
Lewis Murray <lewisdmurray00@gmail.com>
2023-03-02 08:54:18 +0000

    Added the html/jsx required to design the footer, will now add some scss

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { images } from '../../constants';
+import { AppWrap, MotionWrap } from '../../wrapper'
 import './Footer.scss'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <h2 className='head-text'>Come chat with me</h2>
+
+      <div className='app__footer-cards'>
+        <div className='app__footer-card'>
+          <img src={images.email} alt="email" />
+          <a href="mailto:lewisdmurray00@gmail.com" className='p-text'>lewisdmurray00@gmail.com</a>
+        </div>
+      
+        <div className='app__footer-card'>
+          <img src={images.mobile} alt="mobile" />
+          <a href='tel: +447577380000'>07577380000</a>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default Footer
+export default AppWrap(
+  MotionWrap(Footer, 'app__footer'),
+  'contact',
+  'app__whitebg'
+)
